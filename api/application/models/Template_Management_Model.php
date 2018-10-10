89d6a4880d863fa421251bd9bae595f7bf777b58
velz <velmurugan.s@venbainfotech.com>
2018-10-10 19:26:58 +0530

    TEMPALE CHANGED

--- api/application/models/Template_Management_Model.php
+++ api/application/models/Template_Management_Model.php
@@ -10,26 +10,37 @@ class Template_Management_Model extends SPARQ_Model {
         }
 		
 		
-   public function listAllTemplates()
+   public function listAllTemplates($page,$limit,$sort)
    {
-	   $result_data = array();
-	   
+	  $templates = $this->selectRecords(TEMPLATE,array(),$limit,$page,$sort);
 	  
-	   $result = $this->db->get()->result_array();
-	   
-	   if(count($result) > 0 )
-	   {
-		   $result_data['data_status'] = true;
-		   $result_data['data'] = $result;
-		   return $result_data;
+	  if(count($templates))
+	  {
+		  
+		  foreach($templates as $key => $template)
+		  {
+			  $this->db->SELECT('LENDERTEMPLATE.template_lender_map_id, LENDERTEMPLATE.fk_template_id, LENDERTEMPLATE.fk_lender_id,ENTITY.full_name,ENTITY.short_name, LENDERTEMPLATE.fk_product_id,PRODUCTS.name as product_name,PRODUCTS.abbr as product_abbr, LENDERTEMPLATE.fk_customer_segment,CUSTOMERSEGMENT.name as customer_segment_name, CUSTOMERSEGMENT.abbr as customer_segment_abbr,DATE_FORMAT(LENDERTEMPLATE.createdon, "%d/%m/%Y") as createdon,LENDERTEMPLATE.fk_createdby, DATE_FORMAT(LENDERTEMPLATE.updatedon,"%d/%m/%Y") as updatedon, LENDERTEMPLATE.isactive,LENDERTEMPLATE.fk_updatedby,concat(USERPROFILE.first_name," ",USERPROFILE.last_name) as createdby,concat(USERPROFILE1.first_name," ",USERPROFILE1.last_name) as updatedby');
+			  $this->db->FROM(LENDERTEMPLATE.' as LENDERTEMPLATE');
+			  $this->db->JOIN(ENTITY.' as ENTITY','LENDERTEMPLATE.fk_lender_id = ENTITY.entity_id');
+			  $this->db->JOIN(PRODUCTS.' as PRODUCTS','LENDERTEMPLATE.fk_product_id = PRODUCTS.product_id');
+			  $this->db->JOIN(CUSTOMERSEGMENT.' as CUSTOMERSEGMENT','LENDERTEMPLATE.fk_customer_segment = CUSTOMERSEGMENT.customer_segment_id');
+			  $this->db->JOIN(USERPROFILE.' as USERPROFILE','LENDERTEMPLATE.fk_createdby = USERPROFILE.userid');
+			  $this->db->JOIN(USERPROFILE.' as USERPROFILE1','LENDERTEMPLATE.fk_updatedby = USERPROFILE1.userid','LEFT');
+			  $this->db->WHERE('LENDERTEMPLATE.fk_template_id',$template['template_id']);
+			  $child_data = $this->db->GET()->result_array();	
+			  $templates[$key]['lender_details'] = $child_data;
+			  
+		  }
+		  
 		   
-	   }
-	   else
-	   {
-		  $result_data['data_status'] = false;
-		  $result_data['data'] =  null;
-		  return $result_data;
-	   }
+		   return $templates;
+		   
+	   
+	  }
+	  else
+	  {		  
+	   return array();
+	  }
 	   
    }
 
